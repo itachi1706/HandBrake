cc52f991af7a7aabf679a90b055a6dca73c70585
Kenneth Soh <kennethsohyq@gmail.com>
2020-07-27 21:51:43 +0800

    Allow creation of output directories

--- .docker/custom/start.sh
+++ .docker/custom/start.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
 cd /in
-NUMFILES = $(ls -R *.mp4 | wc -l)
+NUMFILES=$(ls -R *.mp4 | wc -l)
 NUM=1
 for i in $(find . -name *.mp4 -type f -print0 | xargs -0)
 do
     echo "Processing ($NUM/$NUMFILES) $i"
     #HandBrakeCLI --preset-import-file /settings/preset.json -i "$i" -o "/out/${i%%.*}.mp4"
+    mkdir -p /out/$(dirname $i)
     HandBrakeCLI --preset-import-file /settings/preset.json -i "$i" -o "/out/${i%.*}.mp4"
     ((NUM++))
 done
