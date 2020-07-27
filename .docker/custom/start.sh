48bd017eb1ab797a10c5de82245c5e2964eab03a
Kenneth Soh <kennethsohyq@gmail.com>
2020-07-27 20:04:19 +0800

    Doing an attempt to support subdirectories as well

--- .docker/custom/start.sh
+++ .docker/custom/start.sh
@@ -3,10 +3,11 @@
 cd /in
 NUMFILES = $(ls *.mp4 | wc -l)
 NUM=1
-for i in *.mp4;
+for i in $(find . -name *.mp4 -type f -print0 | xargs -0)
 do
     echo "Processing ($NUM/$NUMFILES) $i"
-    HandBrakeCLI --preset-import-file /settings/preset.json -i "$i" -o "/out/${i%%.*}.mp4"
+    #HandBrakeCLI --preset-import-file /settings/preset.json -i "$i" -o "/out/${i%%.*}.mp4"
+    HandBrakeCLI --preset-import-file /settings/preset.json -i "$i" -o "/out/${i%.*}.mp4"
     ((NUM++))
 done
 
